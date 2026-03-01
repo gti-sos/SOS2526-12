c60fb93eb915a41490f26f0f09aac4c214db17e0
root <root@DESKTOP-IIUN9UN.localdomain>
2026-03-01 18:39:21 +0100

    FMG index modificado

--- index.js
+++ index.js
@@ -67,6 +67,37 @@ app.get("/samples/JJG", (req, res) => {
     const total = filteredRows.map(row => row.population_age_100).reduce((acc, current) => acc + current, 0);
     const average = filteredRows.length > 0 ? total / filteredRows.length : 0;
 
+
+    app.get("/samples/FMG", (req, res) => {
+    const data = [
+        { country_code: "SI", country_name: "Slovenia", year: 2022, fert_15_19: 7.5, fert_20_24: 56.4 },
+        { country_code: "SI", country_name: "Slovenia", year: 2021, fert_15_19: 8.1, fert_20_24: 55.2 },
+        { country_code: "SI", country_name: "Slovenia", year: 2020, fert_15_19: 7.8, fert_20_24: 54.9 },
+        { country_code: "LG", country_name: "Latvia", year: 2022, fert_15_19: 14, fert_20_24: 54 },
+        { country_code: "MG", country_name: "Mongolia", year: 2022, fert_15_19: 14.7, fert_20_24: 101 },
+        { country_code: "MR", country_name: "Mauritania", year: 2022, fert_15_19: 57.4, fert_20_24: 129.7 },
+        { country_code: "LI", country_name: "Liberia", year: 2022, fert_15_19: 85.5, fert_20_24: 167.9 },
+        { country_code: "TB", country_name: "Saint Barthelemy", year: 2022, fert_15_19: 14.3, fert_20_24: 67 },
+        { country_code: "UP", country_name: "Ukraine", year: 2022, fert_15_19: 25.9, fert_20_24: 89.1 },
+        { country_code: "CD", country_name: "Chad", year: 2022, fert_15_19: 153.8, fert_20_24: 247.6 }
+    ];
+
+    const countryToAnalyze = "Slovenia";
+    const countryData = data.filter(item => item.country_name === countryToAnalyze);
+
+    if (countryData.length > 0) {
+        const sum = countryData
+            .map(item => item.fert_15_19)
+            .reduce((acc, current) => acc + current, 0);
+
+        const average = sum / countryData.length;
+    } else {
+        res.status(404).send(`No se encontraron datos para ${countryToAnalyze}`);
+    }
+});
+
+
+
     res.send(`
         <html>
             <head>
