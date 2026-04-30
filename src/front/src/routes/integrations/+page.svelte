3d8f4916ca45f511e1b932b735ef9473a6f89da4
Javier Jimenez Garcia <javjimgar1@alum.us.es>
2026-04-30 11:11:25 +0200

    integraciones

---
+++ src/front/src/routes/integrations/+page.svelte
@@ -0,0 +1,6 @@
+<h1>Integraciones del Grupo 12</h1>
+<ul>
+    <li><a href="/integrations/mid-population-ages">Integraciones de Javier (Mid Population)</a></li>
+    <li><a href="/integrations/recurso-comp1">Integraciones de [Compañero 1]</a></li>
+    <li><a href="/integrations/recurso-comp2">Integraciones de [Compañero 2]</a></li>
+</ul>
